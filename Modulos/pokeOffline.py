fd8a8d972aaf89eb62ef131b66de1b676b5d5534
PeekoDan <168923088+PeekoDan@users.noreply.github.com>
2024-05-05 20:52:06 -0600

    Update pokeOffline.py

--- Modulos/pokeOffline.py
+++ Modulos/pokeOffline.py
@@ -3,6 +3,7 @@ import json
 #esta función carga un archivo tipo json o txt a una variable
 #lo convierte a una lista, diccionario, etc. que se puede manipular por Python
 def loadFile(fileName):
+  #Daniel Cárdenas Adame
   with open(fileName,'r') as file:
     info=json.load(file)
   #se cierra el archivo
