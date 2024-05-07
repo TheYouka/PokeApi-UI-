fc768615c65e0c8a9c4f114f2a6df3d78de3a9f4
Janrna <168918719+Janrna@users.noreply.github.com>
2024-05-07 16:40:03 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-
+#Graficas
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.chart import Barchart, Reference, Series
+wb=Workbook()
+wb.save('grtip.xlsx')
+wb=load_workbook ('grtip.xslx')
+p1=wb.create_sheet('Pagina 1')
+wb.save('grtip.xslx')
