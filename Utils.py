c8a923c47ad972eb8839deaa14fe09549b1bd774
TheYouka <168247469+TheYouka@users.noreply.github.com>
2024-05-07 19:23:02 -0600

    Modulos para el programa

---
+++ Utils.py
@@ -0,0 +1,10 @@
+def load_json(file):
+	contenido = ""
+
+	with open(file,"r") as f:
+		contenido = f.read()
+
+
+	return contenido
+
+
---
+++ menu_offline.py
@@ -0,0 +1,51 @@
+import json
+import Utils
+
+
+def rewrite_data():
+
+	print("¡Bienvenido al modificador de información\n")
+
+	print("¿Qué sección desea modificar?\n")
+
+	print("1- Pokemones por Color\n\n2-Pokemones por Generación\n\n3-Habitat\n\n4-Grupos de huevos\n\n5-Tipos de pokemones\n")
+
+	opcion = input(">>")
+
+
+
+def By_color():
+	#Convertir Archivo a Diccionario
+	archivo = json.loads(Utils.load_json("Color.txt"))
+
+	#Secciones posibles a modificar
+	opciones = ['negro', 'azul', 'marrón', 'gris', 'verde', 'rosa', 'morado', 'rojo', 'blanco', 'amarillo']
+
+	print( """
+Seleccione el color que desea modificar:
+1. negro
+2. azul
+3. marrón
+4. gris
+5. verde
+6. rosa
+7. morado
+8. rojo
+9. blanco
+10. amarillo
+
+Ingrese el número correspondiente al color que desea modificar: 
+"""
+)
+
+	opcion = input(">> ")
+
+	#Validad datos 
+	pass
+
+By_color()
+
+
+
+
+
